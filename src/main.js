0dfbeca14c0a36856bde280244c776ede6028bf3
Chester <2630538239@qq.com>
2020-01-28 21:15:25 +0800

    create frame

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+// 三方组件按需导入
+import {
+  Tabbar,
+  TabbarItem
+} from 'vant';
+
+// 组件注册
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,92 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- topbar -->
+    <van-nav-bar title="淘兴趣" />
+    <!-- 搜索框 -->
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+    <!-- 轮播图 -->
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+    <!-- 九宫格 -->
+    <van-grid>
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+      <van-grid-item icon="https://img.yzcdn.cn/vant/apple-1.jpg" text="文字" />
+    </van-grid>
+    <!-- 通知消息 -->
+    <van-notice-bar :text="noticeText" left-icon="volume-o" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from "vue";
+import {
+  NavBar,
+  Swipe,
+  SwipeItem,
+  Grid,
+  GridItem,
+  Image,
+  NoticeBar,
+  Search 
+} from "vant";
+
+Vue.use(NavBar)
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(Grid)
+  .use(GridItem)
+  .use(Image)
+  .use(NoticeBar)
+  .use(Search);
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  data() {
+    return {
+      noticeText: "买口罩啦！100元70只，火爆促销！机不可失失不再来！！！！！",
+      value: ''
+    };
+  },
+  methods: {
+    onSearch: function() {
+      
+    },
+    onCancel: function () {
+      
+    }
+  },
+  components: {}
+};
 </script>
+
+<style lang="scss" scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+
+.van-notice-bar {
+  height: 30px;
+}
+</style>
